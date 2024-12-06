bab49d702412df46cac5179c0be94a15d1cc3982
WilliaM <mwilliambarros@gmail.com>
2024-12-06 14:28:57 +0100

    Problema!!

--- Vue/commun/header.php
+++ Vue/commun/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hotel Management</title>
+    <title>Hotel</title>
     <!-- Ajout de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="images/favicon.ico">
@@ -15,15 +15,12 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php?page=accueil">Hotel Management</a>
+                <a class="navbar-brand" href="index.php?page=accueil">Accueil Hotel</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?page=accueil">Accueil</a>
-                        </li>
+                    <ul class="navbar-nav">                        
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?page=inscription">Inscription</a>
                         </li>
